c7d8df2a1ef054bd77986835cc88190ee28b6d50
AdamP-Iot <adam.picka.3@gmail.com>
2026-02-16 12:23:44 +0100

    "Doplněn součet a krácení"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,12 @@ public class Main {
         IO.println("Projektos ZLOMKY");
         IO.println("=======================================");
         var a = new Zlomek(); //Konstruktor bez parametru
-        a.setCitatel(3);
+        a.setCitatel(1);
         a.setJmenovatel(2);
         IO.println("Zlomek: " + a.getCitatel() + "/" + a.getJmenovatel());
-        Zlomek b = new Zlomek(4,5);
+        Zlomek b = new Zlomek(3,4);
         IO.println(b);
+        var soucet = a.plus(b);
+        IO.println(soucet.zkratit());
     }
 }
--- src/main/java/cz/uhk/zlomky/Zlomek.java
+++ src/main/java/cz/uhk/zlomky/Zlomek.java
@@ -1,15 +1,21 @@
 package cz.uhk.zlomky;
+//ctrl + alt + l ==> přeformátování kodu
 
 public class Zlomek {
     private int citatel;
     private int jmenovatel;
+
     /**
      * Basic konstruktor bez parametrů
      */
     public Zlomek() {
+        citatel = 0;
+        jmenovatel = 1;
     }
+
     /**
      * Konstruktor
+     *
      * @param citatel
      * @param jmenovatel musí být různý od nuly
      */
@@ -18,25 +24,78 @@ public class Zlomek {
         this.jmenovatel = jmenovatel;
     }
 
-
     //getry == selektory == čtení dat
-    public int getCitatel(){
+    public int getCitatel() {
         return citatel;
     }
-    public int getJmenovatel(){
+
+    public int getJmenovatel() {
         return jmenovatel;
     }
 
     //setry == modifikátory == změna zápis dat
-    public void setCitatel(int citatel){
+    public void setCitatel(int citatel) {
         this.citatel = citatel;
     }
-    public void setJmenovatel(int jmenovatel){
+
+    public void setJmenovatel(int jmenovatel) {
         this.jmenovatel = jmenovatel;
     }
     //Anotace
+
+    /**
+     * Metoda pro sčítání dvou zlomků
+     *
+     * @param druhy
+     * @return
+     */
+    public Zlomek plus(Zlomek druhy) {
+        int jm = jmenovatel * druhy.getJmenovatel();
+        int cit = citatel * druhy.getJmenovatel() + druhy.citatel * jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    /**
+     * Metoda pro odčítání dvou zlomků
+     *
+     * @param druhy
+     * @return
+     */
+    public Zlomek minus(Zlomek druhy) {
+        int jm = jmenovatel * druhy.getJmenovatel();
+        int cit = citatel * druhy.getJmenovatel() - druhy.citatel * jmenovatel;
+        return new Zlomek(cit, jm);
+    }
+
+    /**
+     * Vypocet největšího společného dělitele čísel a, b
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    private int nsd(int a, int b) {
+        if (a < b) {
+            int pom = a;
+            a = b;
+            b = pom;
+        }
+        int zbytek;
+        do {
+            zbytek = a % b;
+            a = b;
+            b = zbytek;
+        } while (zbytek != 0);
+        return a;
+    }
+
+    public Zlomek zkratit() {
+        int delitel = nsd(citatel, jmenovatel); // vypocet nejv. spol. delitele
+        return new Zlomek(citatel / delitel, jmenovatel / delitel);
+    }
+
     @Override
     public String toString() {
-        return String.format("%3d\n------\n%3d",citatel,jmenovatel); //zarovnání textu
+        return String.format("%d\n/\n%d", citatel, jmenovatel); //zarovnání textu
     }
 }
